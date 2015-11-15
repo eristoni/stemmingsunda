a0184ac5734bdf63cb35074ec489306d69a62508
eris toni <eris toni>
2015-11-16 02:22:32 +0700

    tambah file hello.php

--- hello.php
+++ hello.php
@@ -3,14 +3,14 @@
 	<title> Web app sederhana</title>
 </head>
 <body>
-	<?php>
-if(!empty($_POST))
-{
-	$nama = $_POST['nama'];
-	$email = $_POST['email'];
-	echo '<h1>Halo, '.$nama.'!<h1>';
-	echo 'email :'. $email;
-}
+	<?php
+	if(!empty($_POST))
+	{
+		$nama = $_POST['nama'];
+		$email = $_POST['email'];
+		echo '<h1>Halo, '.$nama.'!<h1>';
+		echo 'email :'. $email;
+	}
 	?>
 </body>
 </html>
