fd06de1d54b21f6583dc9aab18f8d664a4d08d2e
eris toni <eris toni>
2015-11-16 05:05:13 +0700

    edit config

--- config.php
+++ config.php
@@ -3,9 +3,13 @@
 /*
  * All database connection variables
  */
- 
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "katasunda"); // database name
-define('DB_SERVER', "localhost"); // db server
+$hostname = "br-cdbr-azure-south-a.cloudapp.net";
+$username = "bafde986090dc6";
+$password = "f1a2ac88";
+$database_name = "katadasar";
+
+// define('DB_USER', "root"); // db user
+// define('DB_PASSWORD', ""); // db password (mention your db password here)
+// define('DB_DATABASE', "katasunda"); // database name
+// define('DB_SERVER', "localhost"); // db server
 ?>
